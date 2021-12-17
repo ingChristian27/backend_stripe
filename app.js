0282b036f42aa69d2bbd18f07624a2760509b8bf
Christian Duban <cristianoliveros_27@hotmail.com>
2021-12-17 11:14:19 -0500

    other version cors

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ const stripe = new Stripe("sk_test_p3ptDrtq4RpKC7ZxhFMVf9pA");
 //app.use(cors({ origin: "https://driveinlive.us" }));
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.setHeader(
+    "Access-Control-Expose-Headers",
+    "X-Api-Version, X-Request-Id, X-Response-Time"
+  );
+  res.setHeader("Access-Control-Max-Age", "1000");
   next();
 });
 app.use(express.json());
